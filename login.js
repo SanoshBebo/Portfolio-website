ee40c5654445319dff159a338b0fb06db1520cf8
SanoshBebo <sanoshbebo@gmail.com>
2023-09-29 21:38:22 +0530

    darkmode & styles

--- login.js
+++ login.js
@@ -30,30 +30,3 @@ function showLogin() {
   document.getElementById("signup").style.display = "none";
   document.querySelector(".container").style.display = "block";
 }
-
-// function signInWithGoogle() {
-//     const provider = new firebase.auth.GoogleAuthProvider();
-//     firebase
-//       .auth()
-//       .signInWithPopup(provider)
-//       .then((result) => {
-//         // Successful authentication, handle the user data (e.g., store it or redirect to the home page).
-//         const user = result.user;
-//         console.log('Logged in user:', user);
-//       })
-//       .catch((error) => {
-//         // Handle errors here (e.g., display an error message).
-//         console.error('Google Sign-In Error:', error);
-//       });
-//   }
-
-//             // Listen for authentication state changes
-//     firebase.auth().onAuthStateChanged((user) => {
-//         if (user) {
-//         // User is signed in, you can redirect to the home page or perform other actions.
-//         console.log('User is signed in:', user);
-//         } else {
-//         // User is signed out.
-//         console.log('User is signed out.');
-//         }
-//     });
--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode")
+})
+
+// JavaScript to trigger animations when elements are in the viewport
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const elementsToAnimate = document.querySelectorAll('.fade-in-up, .fade-in-right');
+
+function checkElements() {
+  elementsToAnimate.forEach((element) => {
+    if (isElementInViewport(element)) {
+      element.classList.add('animate'); // Add the animation class when in view
+    }
+  });
+}
+
+window.addEventListener('scroll', checkElements);
+
+
